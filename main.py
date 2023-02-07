09ca7dbd72db00b2385dae7ea897e789aba7761c
Maria Laura <mlaura0504bs@gmail.com>
2023-02-07 18:45:17 -0300

    changed somethings in the prototype, therefore in the app

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ class Home(Screen):
     def chama(self):
         print("ola")
 
+
+class Transferir(Screen):
+    pass
+
+class Depositar(Screen):
+    pass
+
 class MiniBanco(MDApp):
 
     def build(self):
         sm = ScreenManager()
         sm.add_widget(SplashScreen(name="splashScreen"))
         sm.add_widget(Home(name="home"))
+        sm.add_widget(Transferir(name="transferir"))
+        sm.add_widget(Depositar(name="depositar"))
+
         return sm
 
 if __name__ == "__main__":
