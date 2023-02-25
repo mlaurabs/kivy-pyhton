4b14efdbbc616df0d7146e19f8e758d6e3985424
mlaurabs <mlaura0504bs@gmail>
2023-02-25 15:10:49 -0300

    starting to work on back-end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import openpyxl
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from os.path import exists
 
 Window.size = (350, 600)
 
@@ -8,8 +10,40 @@ class SplashScreen(Screen):
     pass
 
 class Home(Screen):
-    def chama(self):
-        print("ola")
+
+    def on_enter(self, *args): # ao gera a tela, a função é executada
+        if not exists('Data\dados_base.xlsx'):
+            print("Vou criar o arquivo")
+            dataBase = openpyxl.Workbook()
+            dataBase_sheet = dataBase['Sheet']
+            dataBase_sheet.append(['Valor', 'Data', 'Para', 'Desc', 'Saldo'])
+
+            # deixando em negrito os headers da tabela
+            bold = openpyxl.styles.Font(bold=True)
+            for row in dataBase_sheet["A1:E1"]:
+                for cell in row:
+                    cell.font = bold
+
+            dataBase.save('Data\dados_base.xlsx')
+            dataBase.close()
+        else:
+            print("O arquivo já foi criado")
+
+
+
+"""
+book_base = openpyxl.Workbook()  # creating the excel file we'll be using
+book_base_sheet = book_base['Sheet'] #selecting our sheet
+book_base_sheet.append(['Data', 'Tanque', 'Leitura']) # adding to our sheet data (each word before the comma is a column)
+book_base.save('dados_base.xlsx') # saving and giving the file a name
+book_base.close() # closing our workbook
+
+
+ft = Font(bold=True)
+>>> for row in ws["A1:C1"]:
+...     for cell in row:
+...         cell.font = ft
+"""
 
 
 class Transferir(Screen):
@@ -35,8 +69,8 @@ if __name__ == "__main__":
 """
 Tasks for back-end:
 
-create a function to generate the excel file
-create a function to verificate if there is already an excel file on the directory
+create a function to generate the excel file DONE
+create a function to verify if there is already an excel file on the directory DONE
 create a function to save data into the excel file
 create a function to go through all the file´s data and show it on the recycler view(component)
 create a function to open the excel file
